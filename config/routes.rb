0455d022f6bc1cad371481445e91f6e928cce12c
talha mustafa <mustafatalha222@gmail.com>
2019-12-13 06:33:43 +0500

    electron api

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    namespace :v1 do
+      resources :users
+      post "/match", to: "users#match"
+      get "/match", to: "users#match"
+
+      post "/toggle", to: "users#toggle"
+      get "/toggle", to: "users#toggle"
+
+      post "/logout", to: "users#logout"
+      get "/logout", to: "users#logout"
+    end
+  end
 end
